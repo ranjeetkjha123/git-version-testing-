f80febec13d36bb1ff22b6e6840823a8d94effeb
ranjeetkjha123 <thisisranjeetk@gmail.com>
2018-01-28 14:11:09 +0530

    commit from the master

--- actions/cart.js
+++ actions/cart.js
@@ -4,6 +4,7 @@ import { BUY } from '../constants';
 
 export function buy(product) 
 edited for masterjjjjjjjjj
+final merge by master please try to edit it 
 {
     return { type: BUY, product }
 }
